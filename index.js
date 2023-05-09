b8d5d5f8aa078294764f0a8cd7881c5cb738c980
afmejia23 <andres.moreno@vtex.com.br>
2023-05-09 14:49:07 -0500

    Adding some guardian code

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-$(window).on("orderFormUpdated.vtex", function(evt, orderForm) {
+$(window).on("orderFormUpdated.vtex", function (evt, orderForm) {
+  setTimeout(() => {
+    shippingValue(orderForm);
+  }, 1000);
+});
+
+window.addEventListener("load", function (event) {
+  if ($(".srp-toggle__delivery").hasClass("blue")) {
     setTimeout(() => {
-        shippingValue(orderForm)
+      shippingValue(vtexjs.checkout.orderForm);
     }, 1000);
-})
+  }
 
-window.addEventListener("load", function(event) {
-    if ($(".srp-toggle__delivery").hasClass("blue")) {
-        setTimeout(() => {
-            shippingValue(vtexjs.checkout.orderForm)
-        }, 1000);
-    }
-
-    $( ".srp-toggle__delivery" ).click(function() {
-        setTimeout(() => {
-            shippingValue(vtexjs.checkout.orderForm)
-        }, 1000);
-    }); 
-
-})
+  $(".srp-toggle__delivery").click(function () {
+    setTimeout(() => {
+      shippingValue(vtexjs.checkout.orderForm);
+    }, 1000);
+  });
+});
 
 function shippingValue(orderForm) {
-    if (
-        window.location.hash === "#/cart" &&
-        $('.srp-shipping-current-single__price').length > -1 &&
-        orderForm.shippingData.selectedAddresses != null && 
-        orderForm.totalizers.length > 1 &&
-        $(".srp-toggle__delivery").hasClass("blue")
-    ) {
-        let price = orderForm.totalizers[1].value
-        console.log('esto entra acá')
-        $('#shipping-preview-container .srp-shipping-current-single__price').html('$ ' + formatValue(price))
-    }
+  if (
+    window.location.hash === "#/cart" &&
+    $(".srp-shipping-current-single__price").length > -1 &&
+    orderForm.shippingData.selectedAddresses != null &&
+    orderForm.totalizers.length > 1 &&
+    $(".srp-toggle__delivery").hasClass("blue")
+  ) {
+    let price = orderForm.totalizers[1].value;
+    const locale = orderForm.clientPreferencesData.locale;
+    const currency = orderForm.storePreferencesData.currencyCode;
+    const formattedPrice = formatValue(5, locale, currency) || price;
+
+    $("#shipping-preview-container .srp-shipping-current-single__price").html(
+      "$ " + formattedPrice
+    );
+  }
 }
 
-function formatValue(value) {    
-    let shippingPrice = value.toString().slice(0, -2)
-    shippingPrice = shippingPrice.toLocaleString('es-CL', {
-        style: 'currency',
-        currency: 'CLP',
-      })
-            
-    return shippingPrice
+function formatValue(value, locale, currency) {
+  try {
+    let shippingPrice = value.toString().slice(0, -2);
+    shippingPrice = shippingPrice.toLocaleString(locale, {
+      style: "currency",
+      currency: currency,
+    });
+    return shippingPrice;
+  } catch (error) {
+    console.error(error);
+    return value;
+  }
 }
